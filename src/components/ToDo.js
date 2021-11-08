a079e2d1989498502be76d83657e5997532a1fb9
vid93 <vidmiljanovic@gmail.com>
2021-11-08 23:04:30 +0000

    Added Components

---
+++ src/components/ToDo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Task = () => {
+    return (
+        <div>
+            <h1>
+                Write here!
+            </h1>
+        </div>
+    )
+}
+
+export default Task;
---
+++ src/components/ToDoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const List = () => {
+    return (
+        <div>
+            <h1>
+                This is the list
+            </h1>
+        </div>
+    )
+}
+
+export default List;
